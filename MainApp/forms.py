663ab0c0f2df7d20231b4a2b47d77f0570783fae
MshaFilipushko <ms.ms.ms123@mail.ru>
2025-02-01 04:29:55 +0700

    Обязательный заголовок и картинка

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -7,10 +7,15 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'image']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок (необязательно)'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].required = True
+        self.fields['image'].required = True  # Сделаем изображение обязательным
 
 
 class CommentForm(forms.ModelForm):
